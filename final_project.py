c8276898d54541115f539f6182cc467ed15f70ff
maulanadw <maulanawahyudi701@gmail.com>
2021-07-02 14:26:26 +0700

    mengimport library

--- final_project.py
+++ final_project.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import pandas as pd
+import shap
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.ensemble import RandomForestRegressor
 
 st.write("""
 # Prediksi Harga Rumah App
