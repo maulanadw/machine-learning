30255427f7c2f9dc2d3c51eb5236695ddd1c9dea
maulanadw <maulanawahyudi701@gmail.com>
2021-07-07 09:43:46 +0700

    mengubah import library dan melakukan load dataset

--- final_project.py
+++ final_project.py
@@ -1,13 +1,25 @@
 import streamlit as st
 import pandas as pd
-import shap
 import matplotlib.pyplot as plt
-from sklearn import datasets
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.datasets import load_boston
 
 st.write("""
 # Prediksi Harga Rumah App
 
-Tampilkan prediksi harga perumahan!
+Tampilkan prediksi *harga perumahan*!
 
 """)
+st.write('---')
+
+# Load dan tampilkan datasets
+# Membaca dataset
+dataset = load_boston()
+df = pd.DataFrame(dataset.data)
+# Tetapkan kolom ke df
+df.columns = dataset.feature_names
+# Tetapkan variabel harga rumah
+df["HARGA"] = dataset.target
+
+# Tampilkan data tabel
+if st.checkbox('Tampilkan dataset sebagai tabel'):
+	st.dataframe(df)
